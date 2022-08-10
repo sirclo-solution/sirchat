51f564ee1727530c032727c057056ebf36eda63d
ahmad.mirdas <ahmad.mirdas@sirclo.com>
2022-08-10 09:48:21 +0700

    add comment on folder apps

--- apps/apps.go
+++ apps/apps.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// AppServerConfig as a parameter of a Start()
 type AppServerConfig struct {
 	// Port is specifies the TCP address for the server to listen on,
 	// example (port = 8080)
@@ -15,21 +16,39 @@ type AppServerConfig struct {
 	Timeout int
 }
 
+// Interface App is contract from app struct
+// this wrap method for needed your application
 type App interface {
 	GetAppSecret() string
+
+	// Command is method provide grouping route /command
+	// this method have 2 parameter
+	// first parameter is commandName (ex. /searchProduct)
+	// second parameter is handler, this parameter using type HandlerCommand func(*gin.Context) (interface{}, error)
 	Command(commandName string, handler HandlerCommand)
+
+	// Start use start your service/application
 	Start(param AppServerConfig)
 }
 
+// AppConfig is parameter for initial your Apps
 type AppConfig struct {
+	// AppSecret is secret for verifying request
 	AppSecret string `json:"app_secret"`
 }
 
+// struct app owned by App interface
 type app struct {
-	AppSecret  string `json:"app_secret"`
+	// AppSecret is secret for verifying request
+	AppSecret string `json:"app_secret"`
+
+	// EngineApps used for configuring your application with the server
 	EngineApps *gin.Engine
 }
 
+// setup use to setup application
+// this method it doesn't need to be called in your app, because it's already called here
+// This method includes creating a new server / initial Server
 func (ths *app) setup(cfg AppConfig) error {
 	if cfg.AppSecret == "" {
 		return fmt.Errorf("client Setup(): invalid app secret string")
@@ -44,6 +63,7 @@ func (ths *app) GetAppSecret() string {
 	return ths.AppSecret
 }
 
+// NewApps is method for initial your application
 func NewApps(cfg AppConfig) App {
 	var c app
 	c.setup(cfg)
