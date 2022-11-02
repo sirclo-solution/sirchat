4771d24682be63f976e80e1299d906a6c3cfc54b
ahmad.mirdas <ahmad.mirdas@sirclo.com>
2022-11-02 10:01:23 +0700

    add field view_more and min in text block

--- examples/drawer/drawer.go
+++ examples/drawer/drawer.go
@@ -317,7 +317,9 @@ var cmdExampleThree = func(c context.Context) (interface{}, error) {
 	})
 
 	textBlock2 := models.NewTextBlock(&models.TextBlockObject{
-		Body: "dummy text",
+		Body:     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		ViewMore: true,
+		Min:      100,
 	})
 
 	// example for add new block on container block
