31dbc54fd43d709960270675b1e160de5e3f5af9
barizalhaq <muhammad.bariz@sirclo.com>
2022-11-28 13:48:13 +0700

    feat(container-block): add new fields

--- models/blockContainer.go
+++ models/blockContainer.go
@@ -15,6 +15,14 @@ type ContainerDirection string
 // If the direction is column/vertical, position start is starting from the top until position end is ends on the bottom.
 type ContainerPosition string
 
+// JustifyContentPosition used to aligns the flexible container's items when the items do not use all available space on the main-axis (horizontally)
+// the available positions are start, center, end, and space_between
+type JustifyContentPosition string
+
+// AlignItemsPosition used to specifies the default alignment for items inside the flexible container
+// the available positions are start, center, and end
+type AlignItemsPosition string
+
 const (
 	// CDRow this is a row/horizontal direction
 	CDRow ContainerDirection = "row"
@@ -31,6 +39,27 @@ const (
 
 	// CPEnd this is a final position according to direction.
 	CPEnd ContainerPosition = "end"
+
+	// Items are positioned at the beginning of the container
+	JCPStart JustifyContentPosition = "start"
+
+	// Items are positioned in the center of the container
+	JCPCenter JustifyContentPosition = "center"
+
+	// Items are positioned at the end of the container
+	JCPEnd JustifyContentPosition = "end"
+
+	// Items will have space between them
+	JCPSpaceBetween JustifyContentPosition = "space_between"
+
+	// Items are positioned at the beginning of the container
+	AIPStart AlignItemsPosition = "start"
+
+	// Items are positioned at the center of the container
+	AIPCenter AlignItemsPosition = "center"
+
+	// Items are positioned at the end of the container
+	AIPEnd AlignItemsPosition = "end"
 )
 
 // ContainerBlock is a subtype of block. It represents a container block and holds
@@ -54,6 +83,14 @@ type ContainerBlockObject struct {
 	// Position is optional, position is used when a container is made full height
 	// and the position of the container can be adjusted (start, center, end)
 	Position ContainerPosition `json:"position,omitempty"`
+
+	// JustifyContent is position between container items horizontally
+	// JustifyContent is optional, used to set position between each container items horizontally
+	JustifyContent JustifyContentPosition `json:"justify_content,omitempty"`
+
+	// AlignItems is container items position vertically
+	// AlignItems is optional, used to set container items position vertically
+	AlignItems AlignItemsPosition `json:"align_items,omitempty"`
 }
 
 // Validate performs validation to the ContainerBlock.
@@ -70,6 +107,14 @@ func (ths *containerBlock) Validate() (bool, []error) {
 
 	if positionValid := ths.Container.Position.validateContainerObjectPosition(); !positionValid {
 		errs = append(errs, fmt.Errorf("invalid ContainerPosition %v", ths.Container.Position))
+	}
+
+	if justifyContentValid := ths.Container.JustifyContent.validateContainerJustifyContentPosition(); !justifyContentValid {
+		errs = append(errs, fmt.Errorf("invalid JustifyContent %v", ths.Container.JustifyContent))
+	}
+
+	if alignItems := ths.Container.AlignItems.validateContainerAlignItemsPosition(); !alignItems {
+		errs = append(errs, fmt.Errorf("invalid AlignItems %v", ths.Container.AlignItems))
 	}
 
 	if ths.Container == nil {
@@ -93,8 +138,10 @@ func (ths *containerBlock) Validate() (bool, []error) {
 func NewContainerBlock(containerObj ContainerBlockObject) *containerBlock {
 	obj := ContainerBlockObject{
 		// default direction is column
-		Direction: CDColumn,
-		Position:  containerObj.Position,
+		Direction:      CDColumn,
+		Position:       containerObj.Position,
+		JustifyContent: containerObj.JustifyContent,
+		AlignItems:     containerObj.AlignItems,
 	}
 
 	var block containerBlock
@@ -129,6 +176,24 @@ func (cp ContainerPosition) validateContainerObjectPosition() bool {
 	case CPCenter:
 		return true
 	case CPEnd:
+		return true
+	default:
+		return false
+	}
+}
+
+func (jcp JustifyContentPosition) validateContainerJustifyContentPosition() bool {
+	switch jcp {
+	case "", JCPStart, JCPCenter, JCPEnd, JCPSpaceBetween:
+		return true
+	default:
+		return false
+	}
+}
+
+func (aip AlignItemsPosition) validateContainerAlignItemsPosition() bool {
+	switch aip {
+	case "", AIPStart, AIPCenter, AIPEnd:
 		return true
 	default:
 		return false
